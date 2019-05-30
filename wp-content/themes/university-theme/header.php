715f25647f90052a4f14b717e3eb2163b0200f35
unknown <y.tungmanelatkul@gmail.com>
2019-05-30 18:42:18 +0800

    professor feature up and running

--- wp-content/themes/university-theme/header.php
+++ wp-content/themes/university-theme/header.php
@@ -8,15 +8,15 @@
     <body <?php body_class(); ?>>
     <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo site_url()?>"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url()?>"><strong>Yo</strong> University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
             <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
-            <li><a href="#">Programs</a></li>
-            <li><a href="#">Events</a></li>
+            <li <?php if(get_post_type() == 'program') echo 'class="current-menu-item"' ?>)><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
+            <li <?php if(get_post_type() == 'event' OR is_page('past_events')) echo 'class="current-menu-item"' ?>)><a href="<?php echo get_post_type_archive_link('event')?>">Events</a></li>
             <li><a href="#">Campuses</a></li>
             <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog');?>">Blog</a></li>
           </ul>
--- wp-content/themes/university-theme/page.php
+++ wp-content/themes/university-theme/page.php
@@ -3,16 +3,11 @@
 get_header();
 
 while(have_posts()){
+    pageBanner(array(
+      'title' => 'Hello this is the title',
+      'subtitle' => 'Hi this is the subtitle'
+    ));
     the_post(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">Our History</h1>
-            <div class="page-banner__intro">
-                <p>dont forget to replace me later นะคะ</p>
-            </div>
-        </div>  
-    </div>
 
   <div class="container container--narrow page-section">
 
